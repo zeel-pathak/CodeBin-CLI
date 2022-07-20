868f41d4bdbb542158e0d99762d51b5e6e6d8b0a
Zeel Pathak <zeelpathak2018.it@gmail.com>
2022-07-21 00:02:41 +0530

    Completed the upload part of the CLI tool

--- api/upload.js
+++ api/upload.js
@@ -1,13 +1,20 @@
 import {readFile} from "fs"
+import axios from "axios"
 
 export const uploadFile = (fileLocation) => {
-    readFile(fileLocation,"utf8", (err,data) => {
+    readFile(fileLocation,"utf8", async (err,value) => {
         if(err){
             console.error("Not Able to read file")
             console.log(err.message)
             return
         }
-        const extension = fileLocation.match(new RegExp('[^.]+$'));
-        console.log(data + " " + extension)
+        const extension = await fileLocation.match(new RegExp('[^.]+$'));
+        
+        const params = new URLSearchParams();
+        params.append('value', value);
+        params.append('extension', extension);
+        const result = await axios.post("http://localhost:5000/saveCLI", params)
+
+        console.log(result.data)
     })
 }
