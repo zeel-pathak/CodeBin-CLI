55cad3eac974d70a2424efd3c882c994b358c631
Zeel Pathak <zeelpathak2018.it@gmail.com>
2022-07-22 01:06:22 +0530

    Completed the download file part and made a template for help and other commands other than flags.

--- CLI/cli.js
+++ CLI/cli.js
@@ -2,21 +2,40 @@ import { argv } from 'process';
 import { uploadFile } from '../api/upload.js'
 import { downloadFile } from '../api/download.js'
 
-
-
 export let runner = () => {
 
     switch (argv[2]) {
+
         case "-u":
         
             uploadFile(argv[3])
             break;
 
+        case "upload":
+        
+            uploadFile(argv[3])
+            break;
+                
         case "-d":
         
             downloadFile(argv[3])
             break;
-    
+
+        case "download":
+        
+            downloadFile(argv[3])
+            break;
+
+        case "-h":
+        
+            console.log("It will be helping the devs")    
+            break;
+
+        case "help":
+        
+            console.log("It will be helping the devs")    
+            break;
+                
         default:
             console.log("Wrong flag or expression")
             break;
--- api/download.js
+++ api/download.js
@@ -1,4 +1,35 @@
-export const downloadFile = (codeBinUrl) => {
-    console.log(codeBinUrl)
-    console.log("These will execute the download option")
+import { writeFile } from "fs"
+import axios from "axios"
+
+export const downloadFile = async (codeBinUrl) => {
+
+    if(codeBinUrl.includes("codebinn.herokuapp.com")){
+        
+        const id = codeBinUrl.substr(codeBinUrl.length - 24)
+        
+        if(id.length === 24){
+
+            const params = new URLSearchParams();
+            params.append('id', id);
+            
+            const result = await axios.post("https://codebinn.herokuapp.com/downloadCLI", params)
+            
+            const code = result.data.code;
+            let extension = result.data.extension
+            
+            if(result.data.extension === 'null'){
+                extension = 'txt'
+            }
+            
+            writeFile(`codebin.${extension}`, code, function (err) {
+                if (err) throw err;
+                console.log(`Saved! as codebin.${extension} in current directory`);
+            });
+    
+        }
+    }
+    else{
+        console.log("Url not found")
+    }
+
 }
